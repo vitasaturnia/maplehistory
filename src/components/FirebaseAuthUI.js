3cb36d4b8ce8611bbc4a10afe874fdb2171719d5
tony <officialtony777@Icloud.com>
2023-11-24 04:17:02 -0800

    .

--- src/components/FirebaseAuthUI.js
+++ src/components/FirebaseAuthUI.js
@@ -1,28 +1,46 @@
+// FirebaseAuthUI.js
 import React, { useEffect } from 'react';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebaseui/dist/firebaseui.css';
-import { auth } from '../../firebase'; // Adjust the path as needed
+import { getAuth } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { AuthUI, startUI } from 'firebaseui';
 
 const FirebaseAuthUI = () => {
     useEffect(() => {
+        // Firebase configuration
+        const firebaseConfig = {
+            apiKey: "AIzaSyCo72n2CJWf6WFGDs8jDGDyz07IM3wQZoM",
+            authDomain: "maplehistorymsh.firebaseapp.com",
+            projectId: "maplehistorymsh",
+            storageBucket: "maplehistorymsh.appspot.com",
+            messagingSenderId: "958498273176",
+            appId: "1:958498273176:web:fc15f641ea1db91de79ded",
+            measurementId: "G-BY6NLMDTPJ"
+        };
+
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const auth = getAuth(app);
+        const db = getFirestore(app);
+
         // Firebase UI configuration
         const uiConfig = {
             signInSuccessUrl: '/', // Redirect after sign-in
             signInOptions: [
-                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                {
+                    provider: auth.EmailAuthProvider.PROVIDER_ID,
+                },
                 // You can add other providers here
             ],
             // Configure other options as needed
         };
 
         // Initialize Firebase UI
-        const firebaseui = require('firebaseui');
-        const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
-        ui.start('#firebaseui-auth-container', uiConfig);
+        const ui = new AuthUI(auth);
+        startUI('#firebaseui-auth-container', uiConfig);
 
         // Cleanup UI on unmount
-        return () => ui.delete();
+        return () => ui.reset();
     }, []);
 
     return <div id="firebaseui-auth-container" />;
