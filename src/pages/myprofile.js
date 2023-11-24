0999f93bdfb19e562146445e47463f81b5472cdd
tony <officialtony777@Icloud.com>
2023-11-24 06:10:39 -0800

    .

--- src/pages/myprofile.js
+++ src/pages/myprofile.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import Layout from '../components/Layout'; // Update the path as needed
-import Profile from '../components/Profile'; // Update the path as needed
+import React, { useState } from 'react';
+import { doc, setDoc } from 'firebase/firestore';
+import { updateEmail, updatePassword } from "firebase/auth";
+import { db, auth } from '../../firebase'; //
 
-const ProfilePage = () => {
+const MyProfile = () => {
+    const [username, setUsername] = useState('');
+    const [guildName, setGuildName] = useState('');
+    const [newEmail, setNewEmail] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [error, setError] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+    const handleProfileUpdate = async (e) => {
+        e.preventDefault();
+        try {
+            const userRef = doc(db, "users", auth.currentUser.uid);
+            await setDoc(userRef, { username, guildName }, { merge: true });
+            setSuccessMessage('Profile updated successfully.');
+        } catch (error) {
+            setError('Failed to update profile.');
+        }
+    };
+
+    const handleEmailAndPasswordUpdate = async (e) => {
+        e.preventDefault();
+        try {
+            if (newEmail) {
+                await updateEmail(auth.currentUser, newEmail);
+            }
+            if (newPassword) {
+                await updatePassword(auth.currentUser, newPassword);
+            }
+            setSuccessMessage('Email and/or password updated successfully.');
+        } catch (error) {
+            setError('Failed to update email/password.');
+        }
+    };
+
     return (
-        <Layout>
-            <section className="section">
-                <div className="container">
-                    <Profile />
+        <div>
+            <h1>My Profile</h1>
+            <form onSubmit={handleProfileUpdate}>
+                <div>
+                    <label>Username:</label>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
                 </div>
-            </section>
-        </Layout>
+                <div>
+                    <label>Guild Name:</label>
+                    <input type="text" value={guildName} onChange={(e) => setGuildName(e.target.value)} />
+                </div>
+                <button type="submit">Update Profile</button>
+            </form>
+            <form onSubmit={handleEmailAndPasswordUpdate}>
+                <div>
+                    <label>New Email:</label>
+                    <input type="email" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
+                </div>
+                <div>
+                    <label>New Password:</label>
+                    <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+                </div>
+                <button type="submit">Update Email/Password</button>
+            </form>
+            {error && <p className="error">{error}</p>}
+            {successMessage && <p className="success">{successMessage}</p>}
+        </div>
     );
 };
 
-export default ProfilePage;
+export default MyProfile;
