24a541b1aa5b6d41846f073442fd7e777ca08481
tony <officialtony777@Icloud.com>
2023-11-23 22:55:21 -0800

    .

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,12 @@ import netlifyIdentity from 'netlify-identity-widget';
 
 export const onInitialClientRender = () => {
     netlifyIdentity.init();
+
+    netlifyIdentity.on('login', user => {
+        document.location.href = '/myprofile';
+    });
+
+    netlifyIdentity.on('logout', () => {
+        document.location.href = '/'; // Redirect to home page after logout
+    });
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, navigate } from "gatsby";
 import logo from "../img/fenixlogo2.png";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faDiscord} from "@fortawesome/free-brands-svg-icons";
-import {faUser} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import netlifyIdentity from 'netlify-identity-widget';
-
-
-
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -19,103 +15,95 @@ const Navbar = class extends React.Component {
   }
 
   toggleHamburger() {
-    // toggle the active boolean in the state
     this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: "is-active",
-            })
-          : this.setState({
-              navBarActiveClass: "",
-            });
-      }
+        {
+          active: !this.state.active,
+        },
+        () => {
+          this.state.active
+              ? this.setState({
+                navBarActiveClass: "is-active",
+              })
+              : this.setState({
+                navBarActiveClass: "",
+              });
+        }
     );
   }
 
+  handleProfileClick = () => {
+    const user = netlifyIdentity.currentUser();
+    if (user) {
+      navigate('/myprofile');
+    } else {
+      navigate('/login');
+    }
+  };
+
   render() {
     return (
-      <nav
-        className="navbar is-transparent is-desktop"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="column">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Saturnian" />
-            </Link>
-            {/* Hamburger menu */}
+        <nav
+            className="navbar is-transparent is-desktop"
+            role="navigation"
+            aria-label="main-navigation"
+        >
+          <div className="container">
+            <div className="column">
+              <div className="navbar-brand">
+                <Link to="/" className="navbar-item" title="Logo">
+                  <img src={logo} alt="Saturnian" />
+                </Link>
+                {/* Hamburger menu */}
+                <div
+                    className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+                    data-target="navMenu"
+                    role="menuitem"
+                    tabIndex={0}
+                    onKeyPress={() => this.toggleHamburger()}
+                    onClick={() => this.toggleHamburger()}
+                >
+                  <span />
+                  <span />
+                  <span />
+                </div>
+              </div>
+            </div>
             <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              role="menuitem"
-              tabIndex={0}
-              onKeyPress={() => this.toggleHamburger()}
-              onClick={() => this.toggleHamburger()}
+                id="navMenu"
+                className={`navbar-menu ${this.state.navBarActiveClass}`}
             >
-              <span />
-              <span />
-              <span />
+              <div className="column">
+                <div className="navbar-start has-text-centered">
+                  <Link className="navbar-item" to="/about">
+                    About
+                  </Link>
+                  <Link className="navbar-item" to="/wiki">
+                    Wiki
+                  </Link>
+                  <Link className="navbar-item" to="/scholarships">
+                    Guild
+                  </Link>
+                  <Link className="navbar-item" to="/community">
+                    Community
+                  </Link>
+                  <Link className="navbar-item" to="/forum">
+                    My History
+                  </Link>
+                  <Link className="navbar-item" to="/forum">
+                    Forum
+                  </Link>
+                </div>
+              </div>
+              <div className="column">
+                <div className="navbar-end has-text-centered">
+                  <Link className="navbar-item" to="#" onClick={this.handleProfileClick}>
+                    <FontAwesomeIcon icon={faUser} className="is-size-5 has-text-warning"/>
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="column">
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/wiki">
-                Wiki
-              </Link>
-              <Link className="navbar-item" to="/scholarships">
-                Guild
-              </Link>
-              <Link className="navbar-item" to="/community">
-                Community
-              </Link>
-              <Link className="navbar-item" to="/forum">
-                My History
-              </Link>
-              <Link className="navbar-item" to="/forum">
-                Forum
-              </Link>
-            </div>
-            </div>
-            <div className="column">
-            <div className="navbar-end has-text-centered">
-              <a>
-              <Link to="/myprofile">
-                <span className="icon">
-                  <FontAwesomeIcon icon={faUser} className="socialiconfooter is-size-5 is-size-1-widescreen has-text-warning"/>
-                </span>
-              </Link>
-              </a>
-              <a
-                className="navbar-item"
-                href="https://discord.gg/TcfrdVkdH2"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <FontAwesomeIcon icon={faDiscord} className="socialiconfooter is-size-1-widescreen has-text-warning"/>
-                </span>
-              </a>
-            </div>
-          </div>
-          </div>
-        </div>
-      </nav>
+        </nav>
     );
   }
 };
