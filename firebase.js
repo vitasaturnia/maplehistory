66acfefd3a8a60d97c8a815de54380550f9e4125
tony <officialtony777@Icloud.com>
2023-11-24 02:36:06 -0800

    .

--- firebase.js
+++ firebase.js
@@ -1,18 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"; // Importing Firebase Auth
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCo72n2CJWf6WFGDs8jDGDyz07IM3wQZoM",
-    authDomain: "maplehistorymsh.firebaseapp.com",
-    projectId: "maplehistorymsh",
-    storageBucket: "maplehistorymsh.appspot.com",
-    messagingSenderId: "958498273176",
-    appId: "1:958498273176:web:fc15f641ea1db91de79ded",
-    measurementId: "G-BY6NLMDTPJ"
+    // ... your firebase config
 };
 
 // Initialize Firebase
@@ -25,11 +18,14 @@ if (!getApps().length) {
 
 // Initialize Firebase Analytics
 let analytics;
-if (typeof window !== "undefined") {
+if (typeof window !== 'undefined') {
     analytics = getAnalytics(app);
 }
 
 // Initialize Firestore
 const db = getFirestore(app);
 
-export { db, app, analytics };
+// Initialize Firebase Auth
+const auth = getAuth(app); // Initializing Firebase Auth
+
+export { db, app, analytics, auth }; // Exporting auth
