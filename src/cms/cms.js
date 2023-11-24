7eb7a2f5e1305fa0451740688c966505951c6856
tony <officialtony777@Icloud.com>
2023-11-23 21:17:01 -0800

    .

--- src/cms/cms.js
+++
@@ -1,9 +0,0 @@
-import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
-
-
-
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
-
