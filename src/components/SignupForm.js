f33db21a6231dcafa7b6ee37879b7ab650178bb7
tony <officialtony777@Icloud.com>
2023-11-24 00:24:13 -0800

    .

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,64 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import netlifyIdentity from 'netlify-identity-widget';
 
-const SignupForm = ({ onSuccessfulSignup }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        netlifyIdentity.signup(email, password)
-            .then((user) => {
-                console.log('Signup submission successful:', user);
-                onSuccessfulSignup(); // Call the callback function on successful signup
-            })
-            .catch((error) => {
-                console.error('Error during signup:', error);
-                // Handle signup error here
-            });
+const SignupForm = () => {
+    const handleSignup = () => {
+        netlifyIdentity.open('signup');
     };
 
     return (
         <div className="container">
             <div className="columns is-centered">
                 <div className="column is-half">
-                    <form onSubmit={handleSubmit} className="box">
-                        <div className="field">
-                            <label className="label">Email</label>
-                            <div className="control">
-                                <input
-                                    className="input"
-                                    type="email"
-                                    placeholder="e.g. alex@example.com"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    required
-                                />
-                            </div>
-                        </div>
-
-                        <div className="field">
-                            <label className="label">Password</label>
-                            <div className="control">
-                                <input
-                                    className="input"
-                                    type="password"
-                                    placeholder="*******"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    required
-                                />
-                            </div>
-                        </div>
-
-                        <div className="field">
-                            <div className="control">
-                                <button className="button is-link">
-                                    Sign Up
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    <button onClick={handleSignup} className="button is-link">
+                        Sign Up
+                    </button>
                 </div>
             </div>
         </div>
