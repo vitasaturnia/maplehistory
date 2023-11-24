d67c42396bb376c6372323ae4e6907a494453c5f
tony <officialtony777@Icloud.com>
2023-11-24 03:54:28 -0800

    .

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,44 +1,60 @@
 import React, { useState, useEffect } from 'react';
-import netlifyIdentity from 'netlify-identity-widget';
+import { auth, db } from '../../firebase'; // Import your Firebase configuration
+import { onAuthStateChanged } from 'firebase/auth';
 
 const Profile = () => {
-    const [user, setUser] = useState(netlifyIdentity.currentUser());
-    const [name, setName] = useState('');
+    const [user, setUser] = useState(null);
+    const [profile, setProfile] = useState(null);
 
     useEffect(() => {
-        netlifyIdentity.on('login', user => setUser(user));
-        netlifyIdentity.on('logout', () => setUser(null));
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
 
-        return () => {
-            netlifyIdentity.off('login');
-            netlifyIdentity.off('logout');
-        };
+            if (currentUser) {
+                // If the user is authenticated, fetch their profile data from Firestore
+                const userId = currentUser.uid;
+
+                db.collection('userProfiles')
+                    .where('userId', '==', userId)
+                    .get()
+                    .then((querySnapshot) => {
+                        if (!querySnapshot.empty) {
+                            // Assuming there's only one profile per user
+                            const profileData = querySnapshot.docs[0].data();
+                            setProfile(profileData);
+                        }
+                    })
+                    .catch((error) => {
+                        console.error('Error fetching profile:', error);
+                    });
+            } else {
+                // If the user is not authenticated, reset the profile
+                setProfile(null);
+            }
+        });
+
+        return () => unsubscribe(); // Cleanup subscription on unmount
     }, []);
-
-    const handleSave = () => {
-        user.update({ data: { ...user.user_metadata, name: name } })
-            .then(() => {
-                alert('Profile updated');
-            })
-            .catch((error) => {
-                console.error('Error updating profile: ', error);
-            });
-    };
-
-    if (!user) {
-        return <p>Please log in to view your profile.</p>;
-    }
 
     return (
         <div>
-            <h1>Profile</h1>
-            <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Name"
-            />
-            <button onClick={handleSave}>Save</button>
+            {user ? (
+                <div>
+                    <h2>Your Profile</h2>
+                    {profile ? (
+                        <div>
+                            <p><strong>Ingame Username:</strong> {profile.username}</p>
+                            <p><strong>Guild:</strong> {profile.guild}</p>
+                            <p><strong>Join Date:</strong> {profile.joinDate}</p>
+                            {/* Add more profile fields as needed */}
+                        </div>
+                    ) : (
+                        <p>Loading profile...</p>
+                    )}
+                </div>
+            ) : (
+                <p>Please sign in to view your profile.</p>
+            )}
         </div>
     );
 };
