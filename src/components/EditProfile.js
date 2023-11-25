5cf91ed0f63c898072ff179d09ffb331eec94539
tony <officialtony777@Icloud.com>
2023-11-24 21:51:52 -0800

    .

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,50 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import { auth, db } from '../../firebase'; // Import your Firebase configuration
 import { onAuthStateChanged, updatePassword, updateEmail } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'; // Import Firestore functions
+import { doc, getDoc, setDoc } from 'firebase/firestore'; // Import Firestore functions
 
 const EditProfile = () => {
     const [user, setUser] = useState(null);
     const [profile, setProfile] = useState(null);
+    const [username, setUsername] = useState('');
     const [newIngameUsername, setNewIngameUsername] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [newEmail, setNewEmail] = useState('');
     const [newGuild, setNewGuild] = useState('');
     const [newYearJoined, setNewYearJoined] = useState('');
+    const [newProfilePicture, setNewProfilePicture] = useState(null);
     const [successMessage, setSuccessMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
-    const [isIngameUsernameSet, setIsIngameUsernameSet] = useState(false); // Track whether the ingame username is already set
-    const [isGuildSet, setIsGuildSet] = useState(false); // Track whether the guild is already set
-    const [isYearJoinedSet, setIsYearJoinedSet] = useState(false); // Track whether the year joined is already set
+    const [isIngameUsernameSet, setIsIngameUsernameSet] = useState(false);
+    const [isGuildSet, setIsGuildSet] = useState(false);
+    const [isYearJoinedSet, setIsYearJoinedSet] = useState(false);
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
 
             if (currentUser) {
-                // If the user is authenticated, fetch their profile data from Firestore
-                const userId = currentUser.uid; // Get the UID of the authenticated user
-                const userDocRef = doc(db, 'users', userId); // Use the UID as the document name
+                const userId = currentUser.uid;
+                const userDocRef = doc(db, 'users', userId);
 
                 getDoc(userDocRef)
                     .then((userDocSnapshot) => {
                         if (userDocSnapshot.exists()) {
                             const profileData = userDocSnapshot.data();
                             setProfile(profileData);
+                            setUsername(profileData.username || '');
 
-                            // Check if the ingame username is already set
                             if (profileData.ingameUsername) {
                                 setIsIngameUsernameSet(true);
+                                setNewIngameUsername(profileData.ingameUsername);
                             }
-
-                            // Check if the guild is already set
                             if (profileData.guild) {
                                 setIsGuildSet(true);
+                                setNewGuild(profileData.guild);
                             }
-
-                            // Check if the year joined is already set
                             if (profileData.yearJoined) {
                                 setIsYearJoinedSet(true);
+                                setNewYearJoined(profileData.yearJoined);
                             }
                         }
                     })
@@ -59,14 +59,13 @@ const EditProfile = () => {
 
     const handleUpdateIngameUsername = () => {
         if (user) {
-            // Update the ingame username in Firestore
             const userId = user.uid;
             const userDocRef = doc(db, 'users', userId);
 
             setDoc(userDocRef, { ingameUsername: newIngameUsername }, { merge: true })
                 .then(() => {
                     setSuccessMessage('Ingame Username updated successfully.');
-                    // Update the local profile data if needed
+                    setIsIngameUsernameSet(true);
                     setProfile((prevProfile) => ({
                         ...prevProfile,
                         ingameUsername: newIngameUsername,
@@ -78,9 +77,18 @@ const EditProfile = () => {
         }
     };
 
+    const handleUseIngameUsername = () => {
+        if (profile && profile.ingameUsername) {
+            setUsername(profile.ingameUsername);
+            // Update the username on the server here if necessary
+            setSuccessMessage('Username set to Ingame Username.');
+        } else {
+            setErrorMessage('You did not set IGN yet.');
+        }
+    };
+
     const handleUpdatePassword = () => {
         if (user) {
-            // Update the password using Firebase Authentication
             updatePassword(user, newPassword)
                 .then(() => {
                     setSuccessMessage('Password updated successfully.');
@@ -93,7 +101,6 @@ const EditProfile = () => {
 
     const handleUpdateEmail = () => {
         if (user) {
-            // Update the email address using Firebase Authentication
             updateEmail(user, newEmail)
                 .then(() => {
                     setSuccessMessage('Email address updated successfully.');
@@ -106,14 +113,13 @@ const EditProfile = () => {
 
     const handleUpdateGuild = () => {
         if (user) {
-            // Update the guild in Firestore
             const userId = user.uid;
             const userDocRef = doc(db, 'users', userId);
 
             setDoc(userDocRef, { guild: newGuild }, { merge: true })
                 .then(() => {
                     setSuccessMessage('Guild updated successfully.');
-                    // Update the local profile data if needed
+                    setIsGuildSet(true);
                     setProfile((prevProfile) => ({
                         ...prevProfile,
                         guild: newGuild,
@@ -127,14 +133,13 @@ const EditProfile = () => {
 
     const handleUpdateYearJoined = () => {
         if (user) {
-            // Update the year joined in Firestore
             const userId = user.uid;
             const userDocRef = doc(db, 'users', userId);
 
             setDoc(userDocRef, { yearJoined: newYearJoined }, { merge: true })
                 .then(() => {
                     setSuccessMessage('Year Joined updated successfully.');
-                    // Update the local profile data if needed
+                    setIsYearJoinedSet(true);
                     setProfile((prevProfile) => ({
                         ...prevProfile,
                         yearJoined: newYearJoined,
@@ -146,28 +151,55 @@ const EditProfile = () => {
         }
     };
 
+    const handleUpdateProfilePicture = () => {
+        // Logic to update the profile picture
+        // This will depend on how you handle file uploads and storage
+        // For example, uploading to Firebase Storage and then updating the Firestore user document
+    };
+
     return (
         <div className="section has-text-warning">
             <div className="section has-text-centered">
                 <h3 className="subtitle has-text-warning has-text-weight-bold">Change Login Credentials</h3>
                 {successMessage && <p className="success-message">{successMessage}</p>}
                 {errorMessage && <p className="error-message">{errorMessage}</p>}
+
                 <div>
-                    <h3 className="has-text-weight-bold">Ingame Username</h3>
+                    <h3 className="has-text-weight-bold">Profile Picture</h3>
                     <input
-                        type="text"
-                        value={newIngameUsername}
-                        onChange={(e) => setNewIngameUsername(e.target.value)}
+                        type="file"
+                        onChange={(e) => setNewProfilePicture(e.target.files[0])}
                     />
-                    <br />
                     <div>
-                        {isIngameUsernameSet ? (
-                            <button onClick={handleUpdateIngameUsername} className="button is-warning is-outlined">Update Ingame Username</button>
-                        ) : (
-                            <button onClick={handleUpdateIngameUsername} className="button is-warning is-outlined">Set Ingame Username</button>
-                        )}
+                        <button onClick={handleUpdateProfilePicture} className="button is-warning is-outlined">Update Profile Picture</button>
                     </div>
                 </div>
+
+                <div>
+                    <h3 className="has-text-weight-bold">Username</h3>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+
+                    <div>
+                        <h3 className="has-text-weight-bold">Username</h3>
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <div className="buttons">
+                            <button onClick={handleUpdateIngameUsername} className="button is-warning is-outlined">
+                                {isIngameUsernameSet ? "Change Username" : "Set Username"}
+                            </button>
+                            <button onClick={handleUseIngameUsername} className="button is-warning is-outlined">Use Ingame Username</button>
+                        </div>
+                    </div>
+
+                </div>
+
                 <div>
                     <h3 className="has-text-weight-bold">Password</h3>
                     <input
@@ -175,11 +207,11 @@ const EditProfile = () => {
                         value={newPassword}
                         onChange={(e) => setNewPassword(e.target.value)}
                     />
-                    <br />
                     <div>
                         <button onClick={handleUpdatePassword} className="button is-warning is-outlined">Update Password</button>
                     </div>
                 </div>
+
                 <div>
                     <h3 className="has-text-weight-bold">Email Address</h3>
                     <input
@@ -187,11 +219,25 @@ const EditProfile = () => {
                         value={newEmail}
                         onChange={(e) => setNewEmail(e.target.value)}
                     />
-                    <br />
                     <div>
                         <button onClick={handleUpdateEmail} className="button is-warning is-outlined">Update Email Address</button>
                     </div>
                 </div>
+
+                <div>
+                    <h3 className="has-text-weight-bold">Ingame Username</h3>
+                    <input
+                        type="text"
+                        value={newIngameUsername}
+                        onChange={(e) => setNewIngameUsername(e.target.value)}
+                    />
+                    <div>
+                        <button onClick={handleUpdateIngameUsername} className="button is-warning is-outlined">
+                            {isIngameUsernameSet ? "Change Ingame Username" : "Set Ingame Username"}
+                        </button>
+                    </div>
+                </div>
+
                 <div>
                     <h3 className="has-text-weight-bold">Guild</h3>
                     <input
@@ -199,15 +245,13 @@ const EditProfile = () => {
                         value={newGuild}
                         onChange={(e) => setNewGuild(e.target.value)}
                     />
-                    <br />
                     <div>
-                        {isGuildSet ? (
-                            <button onClick={handleUpdateGuild} className="button is-warning is-outlined">Update Guild</button>
-                        ) : (
-                            <button onClick={handleUpdateGuild} className="button is-warning is-outlined">Set Guild</button>
-                        )}
+                        <button onClick={handleUpdateGuild} className="button is-warning is-outlined">
+                            {isGuildSet ? "Update Guild" : "Set Guild"}
+                        </button>
                     </div>
                 </div>
+
                 <div>
                     <h3 className="has-text-weight-bold">Year Joined</h3>
                     <input
@@ -215,18 +259,17 @@ const EditProfile = () => {
                         value={newYearJoined}
                         onChange={(e) => setNewYearJoined(e.target.value)}
                     />
-                    <br />
                     <div>
-                        {isYearJoinedSet ? (
-                            <button onClick={handleUpdateYearJoined} className="button is-warning is-outlined">Update Year Joined</button>
-                        ) : (
-                            <button onClick={handleUpdateYearJoined} className="button is-warning is-outlined">Set Year Joined</button>
-                        )}
+                        <button onClick={handleUpdateYearJoined} className="button is-warning is-outlined">
+                            {isYearJoinedSet ? "Update Year Joined" : "Set Year Joined"}
+                        </button>
                     </div>
                 </div>
+
             </div>
         </div>
     );
+
 };
 
 export default EditProfile;
