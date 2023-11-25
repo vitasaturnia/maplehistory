246aa72e257a5107940661471572a46a0946a92a
tony <officialtony777@Icloud.com>
2023-11-24 17:19:18 -0800

    .

--- src/components/FirebaseAuthUI.js
+++ src/components/FirebaseAuthUI.js
@@ -1,27 +1,30 @@
 import React, { useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
 import { auth } from '../../firebase'; // Ensure this path is correct
-import firebase from 'firebase/compat/app'; // Ensure Firebase is imported correctly
 
 const FirebaseAuthUI = () => {
     useEffect(() => {
-        // Only execute in the client side
         if (typeof window !== 'undefined') {
-            const firebaseui = require('firebaseui');
-            import('firebaseui/dist/firebaseui.css');
-
             const uiConfig = {
                 signInSuccessUrl: '/',
                 signInOptions: [
-                    firebase.auth.EmailAuthProvider.PROVIDER_ID, // Correct provider ID for email/password
-                    // Add other providers here
+                    firebase.auth.EmailAuthProvider.PROVIDER_ID, // Correct provider ID
                 ],
-                // Additional configuration options as needed
+                callbacks: {
+                    signInSuccessWithAuthResult: (authResult) => {
+                        console.log('Sign-in success:', authResult);
+                        return true; // Return type determines redirect behavior
+                    },
+                    uiShown: () => {
+                        console.log('FirebaseUI Widget rendered');
+                    },
+                },
             };
 
-            let ui = firebaseui.auth.AuthUI.getInstance();
-            if (!ui) {
-                ui = new firebaseui.auth.AuthUI(auth);
-            }
+            const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
             ui.start('#firebaseui-auth-container', uiConfig);
 
             return () => {
