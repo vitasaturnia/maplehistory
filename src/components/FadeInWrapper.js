3bb6b7cf5c389e4c7ebd654370cdf2b61f701052
tony <officialtony777@Icloud.com>
2023-11-25 21:20:57 -0800

    .

--- src/components/FadeInWrapper.js
+++ src/components/FadeInWrapper.js
@@ -1,12 +1,15 @@
-// src/components/FadeInWrapper.js
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
 
 const FadeInWrapper = ({ children }) => {
     const animationProps = useSpring({
-        opacity: 1,
+        to: { opacity: 1 },
         from: { opacity: 0 },
-        config: { duration: 1000 },
+        config: {
+            tension: 200,  // Moderately high for a responsive start
+            friction: 10,  // Balanced for a smooth yet noticeable effect
+            mass: 1,       // Standard mass for natural movement
+        },
     });
 
     return <animated.div style={animationProps}>{children}</animated.div>;
